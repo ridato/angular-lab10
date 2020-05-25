2ac71637b2a9d1ec51cf9b8694db1c31743d4869
unknown <ridato83@gmail.com>
2020-05-25 20:57:39 +0000

    first commit

---
+++ src/app/_models/user.ts
@@ -0,0 +1,12 @@
+﻿
+export class User {
+    id: string;
+    username: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    token: string;
+    age: number;
+    email: string;
+    phone: number;
+}
